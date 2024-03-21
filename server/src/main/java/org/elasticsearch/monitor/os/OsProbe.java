f6ef1af8e3e10ade995b95e69fe858297553fdc0
weiming.chen <weiming.chen@qizhidao.com>
2024-03-21 17:57:11 +0800

    feat: ManagementFactory——java监控和管理JVM

--- server/src/main/java/org/elasticsearch/monitor/os/OsProbe.java
+++ server/src/main/java/org/elasticsearch/monitor/os/OsProbe.java
@@ -552,6 +552,13 @@ public class OsProbe {
     }
 
     private static class OsProbeHolder {
+        //单例
+        //静态内部类在Singleton类被装载时不会立即实例化，而是在需要实例化时，调用getInstance方法，才会装载SingletonInstance类，从而完成Singleton的实例化。
+        //类的静态属性只会在第一次加载类的时候初始化，JVM帮助我们保证了线程的安全性，在类进行初始化时，别的线程无法进入。
+        //优点：避免了线程不安全，延迟加载，效率高。
+        //
+        //链接：https://juejin.cn/post/6992600376411160590
+
         private static final OsProbe INSTANCE = new OsProbe();
     }
 
